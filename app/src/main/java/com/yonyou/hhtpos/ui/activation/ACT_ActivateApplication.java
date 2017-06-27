9c062e53c5ad452bea5b85560181be090dde0093
刘硕菲 <lsf@yonyou.com>
2017-06-27 09:49:57 +0800

    Merge pull request #1 in CANSAAS/canyin-saas-pos from feature/activationfunc-lsf to develop
    Squashed commit of the following:
    
    commit 0a3e7431fe04f50eae64e396ca038e267c281f26
    Author: lsf <lsf@yonyou.com>
    Date:   Tue Jun 27 09:47:17 2017 +0800
    
        1.Add the dimens file.
        2.Add the activate application page.

---
+++ app/src/main/java/com/yonyou/hhtpos/ui/activation/ACT_ActivateApplication.java
@@ -0,0 +1,82 @@
+package com.yonyou.hhtpos.ui.activation;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+import com.yonyou.framework.library.bean.ErrorBean;
+import com.yonyou.framework.library.eventbus.EventCenter;
+import com.yonyou.framework.library.netstatus.NetUtils;
+import com.yonyou.hhtpos.base.ACT_BaseFullScreen;
+
+/**
+ * 作者：liushuofei on 2017/6/26 18:01
+ * 邮箱：lsf@yonyou.com
+ */
+public class ACT_ActivateApplication extends ACT_BaseFullScreen{
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected Fragment getContentFragment() {
+        return new FRA_ActivateApplication();
+    }
+
+    @Override
+    protected boolean isApplyKitKatTranslucency() {
+        return false;
+    }
+
+    @Override
+    protected void getBundleExtras(Bundle extras) {
+
+    }
+
+    @Override
+    protected void onEventComming(EventCenter eventCenter) {
+
+    }
+
+    @Override
+    protected View getLoadingTargetView() {
+        return null;
+    }
+
+    @Override
+    protected void onNetworkConnected(NetUtils.NetType type) {
+
+    }
+
+    @Override
+    protected void onNetworkDisConnected() {
+
+    }
+
+    @Override
+    protected boolean isApplyStatusBarTranslucency() {
+        return false;
+    }
+
+    @Override
+    protected boolean isBindEventBusHere() {
+        return false;
+    }
+
+    @Override
+    protected boolean toggleOverridePendingTransition() {
+        return false;
+    }
+
+    @Override
+    protected TransitionMode getOverridePendingTransitionMode() {
+        return null;
+    }
+
+    @Override
+    public void showBusinessError(ErrorBean error) {
+
+    }
+}
---
+++ app/src/main/java/com/yonyou/hhtpos/ui/activation/FRA_ActivateApplication.java
@@ -0,0 +1,60 @@
+package com.yonyou.hhtpos.ui.activation;
+
+import android.view.View;
+
+import com.yonyou.framework.library.base.BaseFragment;
+import com.yonyou.framework.library.bean.ErrorBean;
+import com.yonyou.framework.library.eventbus.EventCenter;
+import com.yonyou.hhtpos.R;
+
+/**
+ * 作者：liushuofei on 2017/6/26 18:02
+ * 邮箱：lsf@yonyou.com
+ */
+public class FRA_ActivateApplication extends BaseFragment {
+
+    @Override
+    protected void onFirstUserVisible() {
+
+    }
+
+    @Override
+    protected void onUserVisible() {
+
+    }
+
+    @Override
+    protected void onUserInvisible() {
+
+    }
+
+    @Override
+    protected View getLoadingTargetView() {
+        return null;
+    }
+
+    @Override
+    protected void initViewsAndEvents() {
+
+    }
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.fra_activate_application;
+    }
+
+    @Override
+    protected void onEventComming(EventCenter eventCenter) {
+
+    }
+
+    @Override
+    protected boolean isBindEventBusHere() {
+        return false;
+    }
+
+    @Override
+    public void showBusinessError(ErrorBean error) {
+
+    }
+}
