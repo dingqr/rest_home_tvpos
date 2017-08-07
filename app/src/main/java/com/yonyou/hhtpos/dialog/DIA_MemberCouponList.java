504b1c7beb0dbd4307c8e6c0e140213c7e20b2c9
zjuan <honghuotai>
2017-08-07 14:24:49 +0800

    交易记录列表和优惠券列表页面实现

---
+++ app/src/main/java/com/yonyou/hhtpos/dialog/DIA_MemberCouponList.java
@@ -0,0 +1,100 @@
+package com.yonyou.hhtpos.dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.yonyou.hhtpos.R;
+import com.yonyou.hhtpos.adapter.ADA_MemberCoupon;
+import com.yonyou.hhtpos.adapter.CouponEntity;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by zj on 2017/8/7.
+ * 邮箱：zjuan@yonyou.com
+ * 描述：会员-优惠券列表弹窗
+ */
+public class DIA_MemberCouponList implements SwipeRefreshLayout.OnRefreshListener {
+    @Bind(R.id.lv_coupon_list)
+    ListView mListView;
+    @Bind(R.id.tv_total_record_num)
+    TextView tvTotalRecordNum;
+    protected Dialog mDialog;
+    protected View mContentView;
+    protected Context mContext;
+    private ArrayList<CouponEntity> datas = new ArrayList<>();
+    private ADA_MemberCoupon mAdapter;
+
+    public DIA_MemberCouponList(Context context) {
+        mContext = context;
+        mDialog = new Dialog(context, R.style.ActionSheetDialogStyle);
+        mContentView = LayoutInflater.from(context).inflate(R.layout.dialog_transaction_list, null);
+        mDialog.setContentView(mContentView);
+        ButterKnife.bind(this, mContentView);
+        initRecyclerView();
+        setData();
+    }
+
+    /**
+     * 初始化列表View的设置
+     */
+    private void initRecyclerView() {
+
+        mAdapter = new ADA_MemberCoupon(mContext);
+        mListView.setAdapter(mAdapter);
+    }
+
+    /**
+     * 传入数据
+     */
+    private void setData() {
+        for (int i = 0; i < 30; i++) {
+            CouponEntity couponEntity = new CouponEntity();
+            datas.add(couponEntity);
+        }
+        mAdapter.update(datas, true);
+        tvTotalRecordNum.setText("(" + datas.size() + ")");
+    }
+
+    public Dialog show() {
+        mDialog.getWindow().setGravity(Gravity.RIGHT);
+        mDialog.getWindow().setWindowAnimations(R.style.style_right_in_anim);
+        WindowManager.LayoutParams lp = mDialog.getWindow().getAttributes();
+        lp.dimAmount = 0.2f; //背景灰度 -0.0全透明
+        lp.width = 590; // 设置宽度
+        lp.height = 970;//设置高度
+        lp.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        mDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        if (!mDialog.isShowing()) {
+            mDialog.show();
+        }
+        return mDialog;
+    }
+
+    @OnClick({R.id.iv_close})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_close:
+                if (null != mDialog) {
+                    mDialog.dismiss();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+
+    }
+}
--- app/src/main/java/com/yonyou/hhtpos/ui/member/FRA_MemberDetail.java
+++ app/src/main/java/com/yonyou/hhtpos/ui/member/FRA_MemberDetail.java
@@ -6,13 +6,21 @@ import com.yonyou.framework.library.base.BaseFragment;
 import com.yonyou.framework.library.bean.ErrorBean;
 import com.yonyou.framework.library.eventbus.EventCenter;
 import com.yonyou.hhtpos.R;
+import com.yonyou.hhtpos.dialog.DIA_MemberCouponList;
+import com.yonyou.hhtpos.dialog.DIA_TransactionList;
+
+import butterknife.OnClick;
 
 /**
  * Created by zj on 2017/8/4.
  * 邮箱：zjuan@yonyou.com
  * 描述：会员信息详情页面
  */
-public class FRA_MemberDetail extends BaseFragment{
+public class FRA_MemberDetail extends BaseFragment {
+
+    private DIA_TransactionList mDiaTransactionList;
+    private DIA_MemberCouponList mDiaMemberCouponList;
+
     @Override
     protected void onFirstUserVisible() {
 
@@ -35,12 +43,27 @@ public class FRA_MemberDetail extends BaseFragment{
 
     @Override
     protected void initViewsAndEvents() {
-
+        mDiaTransactionList = new DIA_TransactionList(mContext);
+        mDiaMemberCouponList = new DIA_MemberCouponList(mContext);
     }
 
     @Override
     protected int getContentViewLayoutID() {
         return R.layout.fra_member_detail;
+    }
+
+    @OnClick({R.id.tv_check_transaction_records,R.id.ll_coupon})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            //查看交易记录
+            case R.id.tv_check_transaction_records:
+                mDiaTransactionList.show();
+                break;
+            //优惠券
+            case R.id.ll_coupon:
+                mDiaMemberCouponList.show();
+                break;
+        }
     }
 
     @Override
