de7e4ef5b39b33b5d48cb46683b8f97d7c0242bb
JOJO <18510829974@163.com>
2018-07-02 18:50:34 +0800

    监控页面圆环护理进度view处理

--- app/src/main/java/com/smart/tvpos/widgets/PieMarkChatView.java
+++ app/src/main/java/com/smart/tvpos/widgets/PieMarkChatView.java
@@ -10,7 +10,6 @@ import android.graphics.PathEffect;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
-import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -36,13 +35,15 @@ public class PieMarkChatView extends View {
     private boolean mDoTiming = true;
     private Rect textBoundRect;
     private Paint centerTextPaint;
-    private int mCircleRadius = 80;
     private int mWidth;
     private int mHeight;
     //圆心
     private int startX;
     private int startY;
-    private int radius;
+    //圆环刻度的长度
+    private int mCircleKeduWidth = 10;
+    //内圆的半径
+    private int mInnerRadius = 36;
 
     public PieMarkChatView(Context context) {
         this(context, null);
@@ -54,19 +55,19 @@ public class PieMarkChatView extends View {
         setFocusable(true);
         setFocusableInTouchMode(true);
 
-        float x = 160;
-        float y = 100;
-        mShader = new SweepGradient(x, y, new int[]{Color.GREEN,
-                Color.RED,
-                Color.BLUE,
-                Color.GREEN}, null);
+//        float x = 160;
+//        float y = 100;
+//        mShader = new SweepGradient(x, y, new int[]{Color.GREEN,
+//                Color.RED,
+//                Color.BLUE,
+//                Color.GREEN}, null);
         //设置渐变
         //        mPaint.setShader(mShader);
         mPaint.setStyle(Paint.Style.STROKE);
-        //刻度的宽度
-        mPaint.setStrokeWidth(20);
+        mPaint.setStrokeWidth(mCircleKeduWidth);
         mPaint.setAntiAlias(true);
-        PathEffect effect = new DashPathEffect(new float[]{5, 8, 5, 8}, 1);
+        //表盘每个刻度的宽度时6px,间隙为1px
+        PathEffect effect = new DashPathEffect(new float[]{6, 1, 6, 1}, 1);
         //设置画笔path,达到刻度的效果
         mPaint.setPathEffect(effect);
         mPaint.setColor(Color.parseColor("#3662a0"));
@@ -75,7 +76,7 @@ public class PieMarkChatView extends View {
     /**
      * 测量
      */
-    @Override
+//    @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
@@ -84,17 +85,17 @@ public class PieMarkChatView extends View {
         if (widthSpecMode == MeasureSpec.EXACTLY || widthSpecMode == MeasureSpec.AT_MOST) {
             mWidth = widthSpecSize;
         } else {
-            mWidth = 0;
+            mWidth = mInnerRadius * 2 + 2 * mCircleKeduWidth;
         }
         if (heightSpecMode == MeasureSpec.AT_MOST || heightSpecMode == MeasureSpec.UNSPECIFIED) {
-            mHeight = DP2PX.dip2px(mContext, 80);
+            mHeight = mInnerRadius * 2 + 2 * mCircleKeduWidth;
         } else {
             mHeight = heightSpecSize;
         }
         setMeasuredDimension(mWidth, mHeight);
-        startX = mHeight / 2;
-        startY = mWidth / 2;
-        radius = mHeight / 2;
+        mInnerRadius = (mWidth - 2 * mCircleKeduWidth) / 2;
+        startX = mInnerRadius + mCircleKeduWidth;
+        startY = mInnerRadius + mCircleKeduWidth;
     }
 
     @Override
@@ -106,11 +107,10 @@ public class PieMarkChatView extends View {
         centerTextPaint.setColor(Color.parseColor("#68fdfe"));
         centerTextPaint.setTextSize(DP2PX.dip2px(mContext, 20));
 
-//        canvas.drawColor(Color.WHITE);
 
         //80是圆的半径，20为画笔的宽度
-        float x = 80 + 20;
-        float y = 80 + 20;
+//        float x = 80 + 20;
+//        float y = 80 + 20;
 
 //        canvas.drawColor(Color.WHITE);
 
@@ -123,33 +123,18 @@ public class PieMarkChatView extends View {
         //不断绘制，达到动画的效果
 //        invalidate();
         //绘制刻度
-//            for (int i = 0; i < 20; i++) {
-        canvas.drawCircle(x, y, 80, mPaint);
-//            }
+        canvas.drawCircle(startX, startY, mInnerRadius, mPaint);
         mPaint.setColor(Color.parseColor("#2af0ae"));
-//                canvas.drawCircle(x, y, 80, mPaint);
         //20为画笔所占的宽度
-        RectF rectF = new RectF(0 + 20, 20, 160 + 20, 160 + 20); //正方形背景
-        //userCenter true:显示圆中的水平横线
+        RectF rectF = new RectF(0 + mCircleKeduWidth, 0 + mCircleKeduWidth, 2 * mInnerRadius + mCircleKeduWidth, 2 * mInnerRadius + mCircleKeduWidth); //正方形背景
+        //绘制圆弧进度——userCenter true:显示圆中的水平横线
         canvas.drawArc(rectF, 0, 180, false, mPaint); //画弧
-//        else {
-//            RectF rect = new RectF(x - 80, y - 80, x + 80, y + 80);
-//            Paint paintRect = new Paint();
-//            paintRect.setColor(Color.RED);
-//            paintRect.setStyle(Paint.Style.STROKE);
-//            canvas.drawRect(rect, paintRect);
-//            Path path = new Path();
-//            path.addArc(rect, 60, 60);
-//            canvas.clipPath(path);
-//            //canvas.clipPath(path，Op.XOR);
-//            canvas.drawCircle(x, y, 80, paint);
-//        }
 
         //文字的边界矩形
         textBoundRect = new Rect();
         //绘制中心的百分比文字
         String data = 30 + "";
         centerTextPaint.getTextBounds(data, 0, data.length(), textBoundRect);
-        canvas.drawText(data, x - textBoundRect.width() / 2, y + textBoundRect.height() / 2, centerTextPaint);
+        canvas.drawText(data, startX - textBoundRect.width() / 2, startY + textBoundRect.height() / 2, centerTextPaint);
     }
 }
